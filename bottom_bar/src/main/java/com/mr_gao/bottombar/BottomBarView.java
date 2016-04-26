5f0c2b8ee6ccf61aecb1a82244fac01600baa0c6
Mr_gao <787080436@qq.com>
2016-04-26 17:00:04 +0800

    Rejected commit: Default

--- bottom_bar/src/main/java/com/mr_gao/bottombar/BottomBarView.java
+++ bottom_bar/src/main/java/com/mr_gao/bottombar/BottomBarView.java
@@ -2,8 +2,7 @@ package com.mr_gao.bottombar;
 
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
+import android.graphics.drawable.ColorDrawable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,32 +16,34 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
+
 /**
  * Created by Mr_g on 16/4/21.
  */
 public class BottomBarView extends RelativeLayout {
     private static final float SCALEDEFALT = 1;
-    private int NAVIGATION_LINE_WIDTH = (int) getResources().getDimension(com.mr_gao.bottombar.R.dimen.bottom_navigation_height);
-    private Context context;
-    private String bb_active_tvsize;
-    private String bb_inactive_tvsize;
-    private int itemInactiveColor;
-    private FrameLayout container;
-    private LinearLayout item;
     private int currentItem = 0;
-    private List<View> viewList = new ArrayList<>();
-    int navigationWidth = 100;
-
     private int[] imageResources;
     private int[] colorResources;
-    private List<Fragment> fragmentlist;
-    private List<BottomNavigationItem> bottomlistlist;
     private int inactive_color;
     private int active_color;
+
+    public void setOnSelectChanged(SelectChangedListener selectChanged) {
+        this.selectChanged = selectChanged;
+    }
+
+    private int bg_clolor;
+    private int bb_active_tvsize;
+    private int bb_inactive_tvsize;
     private String[] textlist;
     private float SCALELEVEL;
-    private int bg_clolor;
-
+    private Context context;
+    private List<View> viewList = new ArrayList<>();
+    private List<BottomNavigationItem> bottomlistlist;
+    private SelectChangedListener selectChanged;
+    public interface SelectChangedListener {
+        void changeTo(int index);
+    }
 
     public BottomBarView(Context context) {
         this(context, null);
@@ -65,14 +66,13 @@ public class BottomBarView extends RelativeLayout {
 
 
     /**
-     * @param attrs
+     * @param attrs 属性集
      */
     private void initView(AttributeSet attrs) {
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.BottomBarView);
 
         bg_clolor = typedArray.getColor(R.styleable.BottomBarView_bb_bg_color, getResources().getColor(R.color.white));
-        bb_active_tvsize = typedArray.getString(R.styleable.BottomBarView_bb_active_tvsize);
-        bb_inactive_tvsize = typedArray.getString(R.styleable.BottomBarView_bb_inactive_tvsize);
+        bb_inactive_tvsize = typedArray.getDimensionPixelSize(R.styleable.BottomBarView_bb_inactive_tvsize, 12);
         active_color = typedArray.getColor(R.styleable.BottomBarView_bb_active_color, getResources().getColor(R.color.black));
         inactive_color = typedArray.getColor(R.styleable.BottomBarView_bb_inactive_color, getResources().getColor(R.color.black));
         SCALELEVEL = typedArray.getFloat(R.styleable.BottomBarView_bb_scalelevel, (float) 1.2);
@@ -80,15 +80,6 @@ public class BottomBarView extends RelativeLayout {
         typedArray.recycle();
     }
 
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        itemInactiveColor = ContextCompat.getColor(context, R.color.withoutColoredBackground);
-//        ViewGroup.LayoutParams params = getLayoutParams();
-//        params.width = ViewGroup.LayoutParams.MATCH_PARENT;
-//        params.height = NAVIGATION_LINE_WIDTH;
-//        setLayoutParams(params);
-    }
 
     public void setUpWithView(String[] textlist) {
         setUpWithView(null, textlist);
@@ -103,12 +94,6 @@ public class BottomBarView extends RelativeLayout {
         this.colorResources = colorResources;
         this.imageResources = imageResources;
 
-//        if (fragmentlist.size()!= colorResources.length|| fragmentlist.size() != imageResources.length||fragmentlist.size() !=textlist.length)
-//            throw new IllegalArgumentException("colorResources , imageResources   must be equal to the ViewPager items : " + fragmentlist.size());
-//
-//        for (int i = 0; i < textlist.length; i++)
-//            addTab(new BottomNavigationItem(textlist[i], colorResources[i], imageResources[i]));
-
     }
 
     private void addTab(BottomNavigationItem bottomNavigationItem) {
@@ -119,25 +104,21 @@ public class BottomBarView extends RelativeLayout {
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        container = new FrameLayout(context);
+        FrameLayout container = new FrameLayout(context);
         LinearLayout items = new LinearLayout(context);
         items.setOrientation(LinearLayout.HORIZONTAL);
-          container.setBackgroundColor(bg_clolor);
-        ViewGroup.LayoutParams  containerparams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        items.setBackground(new ColorDrawable(getResources().getColor(R.color.white)));
+        container.setBackgroundColor(bg_clolor);
+        ViewGroup.LayoutParams containerparams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
 
         for (int i = 0; i < textlist.length; i++) {
 
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-            item = new LinearLayout(context);
+            LinearLayout item = new LinearLayout(context);
             item.setOrientation(LinearLayout.HORIZONTAL);
             final int index = i;
-//            if (!coloredBackground)
-//                bottomNavigationItems.get(i).setColor(white);
-            int textActivePaddingTop = (int) context.getResources().getDimension(com.mr_gao.bottombar.R.dimen.bottom_navigation_padding_top_active);
-            int viewInactivePaddingTop = (int) context.getResources().getDimension(com.mr_gao.bottombar.R.dimen.bottom_navigation_padding_top_inactive);
-            int viewInactivePaddingTopWithoutText = (int) context.getResources().getDimension(com.mr_gao.bottombar.R.dimen.bottom_navigation_padding_top_inactive_without_text);
-            final View view = inflater.inflate(com.mr_gao.bottombar.R.layout.bottom_navigation, this, false);
+            View view = inflater.inflate(com.mr_gao.bottombar.R.layout.bottom_navigation, this, false);
             ImageView icon = (ImageView) view.findViewById(com.mr_gao.bottombar.R.id.bottom_navigation_item_icon);
             TextView title = (TextView) view.findViewById(com.mr_gao.bottombar.R.id.bottom_navigation_item_title);
             if (i == 0) {
@@ -146,6 +127,7 @@ public class BottomBarView extends RelativeLayout {
                 icon.setScaleX(SCALELEVEL);
                 icon.setScaleY(SCALELEVEL);
                 title.setText(textlist[i]);
+                title.setTextSize(bb_active_tvsize);
                 icon.setImageResource(imageResources[i]);
             } else {
                 title.setTextColor(inactive_color);
@@ -153,32 +135,23 @@ public class BottomBarView extends RelativeLayout {
                 icon.setScaleX(SCALEDEFALT);
                 icon.setScaleY(SCALEDEFALT);
                 title.setText(textlist[i]);
+                title.setTextSize(bb_inactive_tvsize);
                 icon.setImageResource(imageResources[i]);
             }
 
             viewList.add(view);
-//            icon.setImageResource(bottomNavigationItems.get(i).getImageResource());
-//            icon.setColorFilter(i == currentItem ? itemActiveColorWithoutColoredBackground : itemInactiveColor);
             if (i == currentItem) {
-//                container.setBackgroundColor(bottomNavigationItems.get(index).getColor());
-//                title.setTextColor(currentItem == i ?
-//                        itemActiveColorWithoutColoredBackground :
-//                        itemInactiveColor);
                 icon.setScaleX(SCALELEVEL);
                 icon.setScaleY(SCALELEVEL);
             }
 
-//                view.setPadding(view.getPaddingLeft(), i == currentItem ? textActivePaddingTop : withText ? viewInactivePaddingTop : viewInactivePaddingTopWithoutText, view.getPaddingRight(),
-            //                       view.getPaddingBottom());
-//            title.setTextSize(TypedValue.COMPLEX_UNIT_PX, i == currentItem ?
-//                    textActiveSize :
-//                    withText ? textInactiveSize : 0);
 
             LayoutParams itemParams = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
             item.addView(view, itemParams);
             LayoutParams item_params = new LayoutParams(
                     getWidth() / textlist.length, ViewGroup.LayoutParams.MATCH_PARENT);
             items.addView(item, item_params);
+            view.setBackground(new ColorDrawable(bg_clolor));
             view.setOnClickListener(new OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -204,8 +177,6 @@ public class BottomBarView extends RelativeLayout {
     }
 
 
-
-
     private void onBottomNavigationItemClick(int index) {
         if (currentItem == index) {
             return;
@@ -220,6 +191,7 @@ public class BottomBarView extends RelativeLayout {
                 icon.setColorFilter(active_color);
                 scaleView(title, SCALELEVEL);
                 scaleView(icon, SCALELEVEL);
+                title.setTextSize(bb_active_tvsize);
             } else if (currentItem == i) {
                 title.setTextColor(inactive_color);
                 icon.setColorFilter(inactive_color);
@@ -227,16 +199,19 @@ public class BottomBarView extends RelativeLayout {
                 scaleView(icon, SCALELEVEL);
                 icon.setScaleX(SCALEDEFALT);
                 icon.setScaleY(SCALEDEFALT);
+                title.setTextSize(bb_inactive_tvsize);
             }
 
         }
         currentItem = index;
-
+        if (selectChanged!=null){
+            selectChanged.changeTo(index);
+        }
     }
 
     private void scaleView(View view, float scalelevel) {
-        view.setScaleX( scalelevel);
-        view.setScaleY( scalelevel);
+        view.setScaleX(scalelevel);
+        view.setScaleY(scalelevel);
 
     }
 }
